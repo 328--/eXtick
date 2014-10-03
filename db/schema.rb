5b76f9386db7d10add2560ce8c416d155649867d
vagrant <yuya@ie.u-ryukyu.ac.jp>
2014-10-03 22:23:30 +0900

    rake db:create が通らない問題を解決
    boxにてpostgresqlのsuユーザーがvagrantであるため
    config/database.yml の該当箇所を編集

--- db/schema.rb
+++ db/schema.rb
@@ -26,13 +26,4 @@ ActiveRecord::Schema.define(version: 20141001163214) do
     t.datetime "updated_at"
   end
 
-  create_table "users", force: true do |t|
-    t.string   "provider"
-    t.string   "uid"
-    t.string   "screen_name"
-    t.string   "name"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
 end
