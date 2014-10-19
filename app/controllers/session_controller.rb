ce17b37777cd8d84fef4c27e81f8df5b0c6eeb14
vagrant <k148574@ie.u-ryukyu.ac.jp>
2014-10-19 19:38:11 +0900

    fix via. If provided token and secret != db's token and secret, update db.

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -2,6 +2,11 @@ class SessionController < ApplicationController
   def callback
     auth = request.env['omniauth.auth']
     user = User.find_by_provider_and_uid(auth[:provider], auth[:uid]) || User.create_with_omniauth(auth)
+
+    # check token. if provided token and secret != db's token and secret, update db.
+    if (user.token != auth.credentials.token) || (user.secret != auth.credentials.secret)
+      user.update(token: auth.credentials.token, secret: auth.credentials.secret)
+    end
     session[:uid] = user.uid
     session[:name] = user.screen_name
     redirect_to(controller: "tickets")
