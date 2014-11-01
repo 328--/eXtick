6f83ae7bfd4ea9bd4107b4bb30ce102302fd4b0c
yasu <spitzyasu@gmail.com>
2014-11-01 12:41:21 +0900

    remove migration files

--- db/migrate/20141027144922_remove_columns_from_tickets.rb
+++
@@ -1,7 +0,0 @@
-class RemoveColumnsFromTickets < ActiveRecord::Migration
-  def change
-    remove_column :tickets, :category_id, :integer
-    remove_column :tickets, :tag_ids, :text
-    remove_column :tickets, :twitter_token, :string
-  end
-end
