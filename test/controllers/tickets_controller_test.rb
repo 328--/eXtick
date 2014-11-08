87f065b6bccc664b9fde038afdf1e87385e2b812
vagrant <k148574@ie.u-ryukyu.ac.jp>
2014-11-08 14:18:36 +0900

    delete unnecessary test

--- test/controllers/tickets_controller_test.rb
+++ test/controllers/tickets_controller_test.rb
@@ -8,33 +8,12 @@ class TicketsControllerTest < ActionController::TestCase
     @params = {"categories"=>nil, "tags"=>""}
   end
 
-  test "should get index" do
-    get :index
-    assert_response :success
-    assert_not_nil assigns(:tickets)
-  end
-
-  test "should be redirected by new when user did not login" do
-    get :new
-    assert_response :redirect
-  end
-
   test "should create ticket" do
     assert_difference('Ticket.count') do
       post(:create, ticket: @ticket_params, params: @params)
     end
 
     assert_redirected_to ticket_path(assigns(:ticket))
-  end
-
-  test "should show ticket" do
-    get :show, id: @ticket
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get :edit, id: @ticket
-    assert_response :success
   end
 
   # test "should update ticket" do
