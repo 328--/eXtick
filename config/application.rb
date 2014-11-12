3f7dbffaf3e6bb594f60295261b81723c826c9f2
vagrant <k148574@ie.u-ryukyu.ac.jp>
2014-11-12 20:16:11 +0900

    parsley localize

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,8 @@ module EXtick
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :ja
+
+    config.assets.precompile += ["ja", "en"].map { |l| "parsley.i18n.#{l}.js" }
+
   end
 end
