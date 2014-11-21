6d552a72096f6cc7f9f425c16d2397c83ef8650a
328-- <yuya@ie.u-ryukyu.ac.jp>
2014-11-21 18:01:23 +0900

    bug fix

--- lib/eventmachine/cron.rb
+++ lib/eventmachine/cron.rb
@@ -6,12 +6,10 @@ module Clockwork
   handler do |job|
     case job
     when 'ticket_bot_tweet_delete'
-      day = Date::today-4
-#      day = Date::today-7
+      day = Date::today-7
       TicketBot.destroy_all(["created_at <= ? ",day])
     end
   end
 
-#  every(1.day, 'ticket_bot_tweet_delete', :at => '05:00')
-  every(10.second, 'ticket_bot_tweet_delete')
+  every(1.day, 'ticket_bot_tweet_delete', :at => '05:00')
 end
