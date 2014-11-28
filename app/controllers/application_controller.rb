db08621c549bc11aa7a4197d84ce62aac2e42f79
vagrant <k148574@ie.u-ryukyu.ac.jp>
2014-11-29 00:06:07 +0900

    404 render

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  rescue_from ActiveRecord::RecordNotFound, with: :render_404
+  rescue_from ActionController::RoutingError, with: :render_404  
+
+  def render_404
+    render status: 404, template: "errors/404"
+  end
+
 end
