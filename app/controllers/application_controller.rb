adccc89e7c3b58887d94de9e2fdbe45d3a832e34
vagrant <k148574@ie.u-ryukyu.ac.jp>
2014-11-29 00:18:52 +0900

    500 render

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,16 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   rescue_from ActiveRecord::RecordNotFound, with: :render_404
   rescue_from ActionController::RoutingError, with: :render_404  
+  unless Rails.env.development?
+    rescue_from Exception, with: :render_500
+  end
 
   def render_404
     render status: 404, template: "errors/404"
+  end
+
+  def render_500
+    render status: 500, template: "errors/500"
   end
 
 end
